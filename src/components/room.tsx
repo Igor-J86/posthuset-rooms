8967c3998e42a40e5da0e2ead6eafe5ec802f347
Igor Jorganovic <igor.jorganovic@bring.com>
2025-01-16 20:05:32 +0100

    Add raycast on Texts and format room file

--- src/components/room.tsx
+++ src/components/room.tsx
@@ -1,7 +1,7 @@
-import { Mesh } from "three"
-import { Text } from "@react-three/drei"
-import type { RoomProps } from "../utils/types"
-import { colors } from "../utils/globals"
+import { Mesh } from "three";
+import { Text } from "@react-three/drei";
+import type { RoomProps } from "../utils/types";
+import { colors } from "../utils/globals";
 
 const Room: React.FC<RoomProps> = ({
   number,
@@ -25,15 +25,27 @@ const Room: React.FC<RoomProps> = ({
   >
     <meshStandardMaterial color={selected ? colors.selected : colors.default} />
     <boxGeometry args={dimensions} />
-    <Text position={[0, 0.2, 0.12]} fontSize={0.3} color={'#111111'} scale={1.5}>
+    <Text
+      position={[0, 0.2, 0.12]}
+      fontSize={0.3}
+      color={"#111111"}
+      scale={1.5}
+      raycast={() => null}
+    >
       {number}
     </Text>
-    {name &&
-      <Text position={[0, number ? -0.3 : 0, 0.12]} fontSize={0.2} color={'#111111'} scale={1.5}>
+    {name && (
+      <Text
+        position={[0, number ? -0.3 : 0, 0.12]}
+        fontSize={0.2}
+        color={"#111111"}
+        scale={1.5}
+        raycast={() => null}
+      >
         {name}
       </Text>
-    }
+    )}
   </mesh>
-)
+);
 
-export default Room
+export default Room;
